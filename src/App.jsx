bb131f53b219b33788bfa0d870df496c8460f1ab
PGS <sarangipriyanshu@gmail.com>
2023-04-14 13:26:49 +0530

    commit fc

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import About from './components/About/About'
 import Experience from './components/Experience/Experience'
-import Services from './components/Services/Services'
+ 
 import Portfolio from './components/Portfolio/Portfolio'
-import Testimonials from './components/Testimonials/Testimonials'
+ 
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 
@@ -22,11 +22,8 @@ const App = () => {
     <Navbar />
     <About />
     <Experience />
-    <Services />
     < Portfolio />
-    <Testimonials />
     <Contact />
-   
    <Footer/>
    </div>
     
